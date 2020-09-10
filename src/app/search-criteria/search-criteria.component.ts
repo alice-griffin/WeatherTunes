b9d3caeb99db95b8c0e814a4cd5bc2a025cf81f6
Nicole <nbush89@gmail.com>
2020-09-10 19:38:30 -0400

    updated getplaylist method in the service to accept weather params

--- src/app/search-criteria/search-criteria.component.ts
+++ src/app/search-criteria/search-criteria.component.ts
@@ -48,84 +48,15 @@ export class SearchCriteriaComponent implements OnInit {
         this.artist = d;
         this.artistId = d.artists.items[0].id;
         let params = {
-            artist: this.artistId,
-          };
-        this.getPlaylistParameters('Mist', params);
-        this.spotifyService.getPlaylist(params).subscribe((res: any) => {
-          this.spotifyService.playlist = res;
-          console.log(res);
-        });
+          artist: this.artistId,
+        };
+        this.spotifyService
+          .getPlaylist(this.weatherDesc, params)
+          .subscribe((res: any) => {
+            this.spotifyService.playlist = res;
+            console.log(res);
+          });
       });
     });
-  }
-
-  getPlaylistParameters(weather: string, parameters: any) {
-    switch (weather) {
-      case 'Mist':
-        parameters.target_energy = 0.3;
-        parameters.target_acousticness = 1.0; 
-        break;
-      case 'Thunderstorm':
-        parameters.target_energy = 0.6;
-        parameters.target_mode = 0; 
-        break;
-      case 'Drizzle':
-        parameters.target_energy = 0.4;
-        parameters.target_instrumentalness = 1.0; 
-        break;
-      case 'Rain':
-        parameters.target_energy = 0.5;
-        parameters.target_instrumentalness = 1.0; 
-        break;
-      case 'Snow':
-        parameters.target_energy = 0.2; 
-        break;
-      case 'Clear':
-        parameters.target_danceability = 1.0;
-        parameters.target_popularity = 100; 
-        break;
-      case 'Clouds':
-        parameters.target_energy = 0.5;
-        parameters.target_acousticness = 0.5
-        break;
-      case 'Rainy':
-        parameters.target_energy = 0.2;
-        parameters.target_acousticness = 1.0; 
-        break;
-      case 'Smoke':
-        parameters.target_energy = 0.8;
-        parameters.target_danceability = 0.0;
-        break;
-      case 'Haze':
-        parameters.target_energy = 0.3;
-        parameters.target_acousticness = 1.0; 
-        break;
-      case 'Dust':
-        parameters.target_energy = 0.6;
-        parameters.target_min_tempo = 150; 
-        break;
-      case 'Fog':
-        parameters.target_energy = 0.3;
-        parameters.target_danceability = 0.4; 
-        break;
-      case 'Sand':
-        parameters.target_energy = 1;
-        parameters.target_danceability = 1;
-        break;
-      case 'Ash':
-        parameters.target_energy = 0.8;
-        parameters.target_danceability = 0;
-        break;
-      case 'Squall':
-        parameters.target_energy = 0.7;
-        parameters.target_danceability = 0.7;
-        break;
-      case 'Tornado':
-        parameters.target_energy = 1.0;
-        parameters.target_danceability = 0.4;
-        break;
-      default:
-        console.log('default hit');
-    }
   }
 }
--- src/app/spotify.service.ts
+++ src/app/spotify.service.ts
@@ -3,47 +3,217 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SpotifyService {
-
   token: string;
   clientId: string = 'd0502d4fc5a9406b8afda0decad6a3d0';
   clientSecret: string = '6f9fdb44e4bf49bda20513dd4399bff6';
-  playlist: any[]; 
-  artist: any; 
+  playlist: any[];
+  artist: any;
   artistId: any;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getToken() {
-    return this.http.post(`https://accounts.spotify.com/api/token`, `grant_type=client_credentials`, {
-      headers: {
-        Authorization: 'Basic ' + btoa(this.clientId + ':' + this.clientSecret),
-        'Content-Type': 'application/x-www-form-urlencoded'
+    return this.http.post(
+      `https://accounts.spotify.com/api/token`,
+      `grant_type=client_credentials`,
+      {
+        headers: {
+          Authorization:
+            'Basic ' + btoa(this.clientId + ':' + this.clientSecret),
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
       }
-    });
+    );
   }
-  getPlaylist(data: any): Observable<any> {
-    let params = {
-      limit: data.limit ? data.limit : 10,
-      seed_artists: data.artist ? data.artist : '',
-      seed_genres: data.genres ? data.genres : '',
-      target_valence: data.target_valence ? data.target_valence: null,
-      target_energy: data.target_energy ? data.target_energy: null,
-      target_danceability: data.target_danceability ? data.target_valence: null,
-      target_acousticness: data.target_acousticness ? data.target_acousticness: null,
-      target_instrumentalness: data.target_instrumentalness ? data.target_instrumentalness: null
+  getPlaylist(weather: string, data: any): Observable<any> {
+    let params;
+    switch (weather) {
+      case 'Mist':
+        params = {
+          target_energy: 0.3,
+          target_acousticness: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Thunderstorm':
+        params = {
+          target_energy: 0.6,
+          target_mode: 0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Drizzle':
+        params = {
+          target_energy: 0.4,
+          target_instrumentalness: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Rain':
+        params = {
+          target_energy: 0.5,
+          target_instrumentalness: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Snow':
+        params = {
+          target_energy: 0.2,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Clear':
+        params = {
+          target_danceability: 1.0,
+          target_popularity: 100,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Clouds':
+        params = {
+          target_energy: 0.5,
+          target_acousticness: 0.5,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Rainy':
+        params = {
+          target_energy: 0.2,
+          target_acousticness: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Smoke':
+        params = {
+          target_energy: 0.8,
+          target_danceability: 0.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Haze':
+        params = {
+          target_energy: 0.3,
+          target_acousticness: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Dust':
+        params = {
+          target_energy: 0.6,
+          target_min_tempo: 150,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Fog':
+        params = {
+          target_energy: 0.3,
+          target_danceability: 0.4,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Sand':
+        params = {
+          target_energy: 1.0,
+          target_danceability: 1.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Ash':
+        params = {
+          target_energy: 0.8,
+          target_danceability: 0.0,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Squall':
+        params = {
+          target_energy: 0.7,
+          target_danceability: 0.7,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      case 'Tornado':
+        params = {
+          target_energy: 1.0,
+          target_danceability: 0.4,
+          limit: data.limit ? data.limit : 10,
+          seed_artists: data.artist ? data.artist : '',
+          seed_genres: data.genres ? data.genres : '',
+          target_valence: data.valence ? data.valence : 0.5,
+        };
+        break;
+      default:
+        console.log('default hit');
     }
-    return this.http.get('https://api.spotify.com/v1/recommendations', {params: params, headers: {Authorization: this.token}});
+    return this.http.get('https://api.spotify.com/v1/recommendations', {
+      params: params,
+      headers: { Authorization: this.token },
+    });
   }
 
   getArtist(data: any): Observable<any> {
-    return this.http.get('https://api.spotify.com/v1/search', {params: {q: data.q ? data.q: '', type: "artist", limit: data.limit ? data.limit : 1}, headers: {Authorization: this.token}});
+    return this.http.get('https://api.spotify.com/v1/search', {
+      params: {
+        q: data.q ? data.q : '',
+        type: 'artist',
+        limit: data.limit ? data.limit : 1,
+      },
+      headers: { Authorization: this.token },
+    });
   }
 
   getGenres() {
-    return this.http.get('https://api.spotify.com/v1/recommendations/available-genre-seeds', {headers: {Authorization: this.token}})
+    return this.http.get(
+      'https://api.spotify.com/v1/recommendations/available-genre-seeds',
+      { headers: { Authorization: this.token } }
+    );
   }
-
 }
